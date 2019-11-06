11604d9ed0667245ad23437a983babfef8728600
91998 <91998@192.168.0.7>
2019-11-06 15:06:09 +0530

    static import

---
+++ corejava/src/corejava/StaticImportTest.java
@@ -0,0 +1,14 @@
+package corejava;
+
+import static java.lang.System.out;
+import static java.lang.Math.PI;
+
+public class StaticImportTest {
+
+	public static void main(String[] args) {
+		out.println("using static import");
+		out.println(PI);
+
+	}
+
+}
