c0a80df1cbf26c4f513b6bf0282c95dd15d8c4e3
91998 <91998@192.168.0.7>
2019-11-06 15:05:49 +0530

    Inner  classes

---
+++ corejava/src/innerclasses/TLCTest.java
@@ -0,0 +1,24 @@
+package innerclasses;
+class TLC {
+	static public class SMC {
+		public void paint() {
+			System.out.println("In SMC");
+		}
+	}
+
+	public void print() {
+		System.out.println("In TLC");
+	}
+}
+
+public class TLCTest {
+
+	public static void main(String[] args) {
+		TLC.SMC obj = new TLC.SMC();
+		obj.paint();
+		
+		TLC outerObj=new TLC();
+		outerObj.print();
+	}
+
+}
